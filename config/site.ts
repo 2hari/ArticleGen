b86ff587d5a07aa21d545370ab9e4d8121449200
2hari <two1821125run@protonmail.com>
2023-05-04 11:47:39 +0300

    Initial commit from Create Next App

---
+++ config/site.ts
@@ -0,0 +1,8 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "ArticleGen",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  mainNav: [],
+}
